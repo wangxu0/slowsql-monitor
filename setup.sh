e4777a9ff110e83bce5180c51b52c24b819478e1
wxisme <850885154@qq.com>
2018-03-15 18:36:10 +0800

    fix setup bash

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 monitor_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )""/monitor/monitor.py"
 
-echo "0 8 * * 1"${monitor_path} >> /var/spool/cron/root
+echo "0 8 * * 1 python "${monitor_path} >> /var/spool/cron/root
 
 /etc/init.d/cron stop
 /etc/init.d/cron start
